324f74b868c51b2da5fdcd104718623d3378ff78
hanifan.nurahman <hanifan.nurahman@ibm.com>
2023-11-19 15:04:24 +0700

    initial commit: adding some previous solution

---
+++ neetcode/arrays_and_hashing/242_valid_anagram.rs
@@ -0,0 +1,54 @@
+fn main() {
+    let s = String::from("anagram");
+    let t = String::from("nagaram");
+
+    println!("{}", Solution::is_anagram(s, t));
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let mut res = true;
+
+        if s.len() != t.len() {
+            res = false;
+        } else {
+            let letters = String::from("abcdefghijklmnopqrstuvwxyz");
+            let s_vec: Vec<char> = s.chars().collect();
+            let t_vec: Vec<char> = t.chars().collect();
+
+            for letter in letters.chars() {
+                if s_vec.iter().filter(|&n| *n == letter).count()
+                    != t_vec.iter().filter(|&n| *n == letter).count()
+                {
+                    res = false;
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Solution;
+
+    #[test]
+    fn true_test() {
+        let s = String::from("hanifan");
+        let t = String::from("nafinah");
+
+        assert_eq!(Solution::is_anagram(s, t), true);
+    }
+
+    #[test]
+    fn false_test() {
+        let s = String::from("yow");
+        let t = String::from("hai");
+
+        assert_eq!(Solution::is_anagram(s, t), false);
+    }
+}
